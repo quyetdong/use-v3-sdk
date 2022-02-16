f30c80fa2e8ea38d142a7b11b6a75d5dbfc6505e
QuyetDong <quyetvdong@gmail.com>
2022-02-16 15:40:27 +0700

    update cal price

--- script.ts
+++ script.ts
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 import { Pool } from "@uniswap/v3-sdk";
 import { Token } from "@uniswap/sdk-core";
 import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+import { abi as IERC20ABI } from "@openzeppelin/contracts/build/contracts/IERC20Metadata.json";
 import { BigNumber } from "ethers";
 
 // user infura endpoint to query chain data from ethereum
@@ -82,6 +83,36 @@ async function getPoolImmutables() {
     poolContract.maxLiquidityPerTick(),
   ]);
 
+  // console.log('*** token0', token0);
+
+  const token0Contract = new ethers.Contract(
+    token0,
+    IERC20ABI,
+    provider
+  );
+  const token1Contract = new ethers.Contract(
+    token1,
+    IERC20ABI,
+    provider
+  );
+  // const token0Supply = await token0Contract.totalSupply();
+  // console.log('*** token0', String(token0Supply));
+  const [token0S, token0N, token0Dec] = await Promise.all([
+    token0Contract.symbol(),
+    token0Contract.name(),
+    token0Contract.decimals()
+  ]);
+
+  const [token1S, token1N, token1Dec] = await Promise.all([
+    token1Contract.symbol(),
+    token1Contract.name(),
+    token1Contract.decimals()
+  ]);
+
+  console.log('*** token0', token0S, token0N, token0Dec);
+  console.log('*** token1', token1S, token1N, token1Dec);
+  
+
   const immutables: Immutables = {
     factory,
     token0,
@@ -103,6 +134,7 @@ async function getPoolState() {
   const PoolState: State = {
     liquidity,
     sqrtPriceX96: slot[0],
+    // sqrtPriceX96: slot.sqrtPriceX96,
     tick: slot[1],
     observationIndex: slot[2],
     observationCardinality: slot[3],
@@ -111,6 +143,7 @@ async function getPoolState() {
     unlocked: slot[6],
   };
 
+  // console.log('*** slot ', slot);
   return PoolState;
 }
 
@@ -119,13 +152,14 @@ async function getPoolState() {
 //   console.log(result);
 // });
 
-getPoolState().then((result) => {
-  console.log(result);
 
-  console.log(String((result.sqrtPriceX96
-    .div(BigNumber.from(2).pow(96)))
-    .pow(2)))
-});
+// getPoolState().then((result) => {
+//   // console.log(result);
+
+//   console.log(String((result.sqrtPriceX96
+//     .div(BigNumber.from(2).pow(96)))
+//     .pow(2)))
+// });
 
 async function main() {
   const [immutables, state] = await Promise.all([
@@ -133,8 +167,8 @@ async function main() {
     getPoolState(),
   ]);
 
-  const TokenA = new Token(3, immutables.token0, 6, 'USDC', 'USD Coin Ex');
-  const TokenB = new Token(3, immutables.token1, 18, 'WETH', 'Wrapped Ether Ex');
+  const TokenA = new Token(3, immutables.token0, 6, 'USDC', 'USD Coin');
+  const TokenB = new Token(3, immutables.token1, 18, 'WETH', 'Wrapped Ether');
 
   const poolExample = new Pool(
     TokenA,
@@ -145,7 +179,26 @@ async function main() {
     state.tick
   );
 
-  console.log(poolExample);
+
+  // console.log(String((state.sqrtPriceX96
+  //   .div(BigNumber.from(2).pow(96)))
+  //   .pow(2)))
+
+
+  const numer = BigNumber.from(String(poolExample.token1Price.numerator));
+  const deno = BigNumber.from(String(poolExample.token1Price.denominator));
+  const rawPrice = numer.mul(BigNumber.from(10).pow(18)).div(deno).toNumber();
+  // const modPrice = numer.mul(Math.pow(10, 18)).mod(deno);
+
+  // const floatN = modPrice/(deno.toNumber());
+
+  // console.log(String(poolExample.token0Price.numerator));
+  // console.log(String(poolExample.token0Price.denominator));
+  console.log(rawPrice/Math.pow(10, 6));
+  // console.log(String(modPrice));
+  // console.log(String(floatN));
+
+  // console.log(String(numer.div(deno)));
 }
 
-// main();
+main();
